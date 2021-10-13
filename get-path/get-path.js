b5e48e768f5fda7592aa932c4d339f67d7b94b37
Ilya Abramovich <ilyaabramovich1998@gmail.com>
2021-10-13 23:50:01 +0300

    fix after review

--- get-path/get-path.js
+++ get-path/get-path.js
@@ -7,17 +7,31 @@ module.exports = function getPath(elem) {
     throw new TypeError('Expected a valid DOM element as an argument')
   }
 
-  while (currentElem.parentNode.tagName !== 'HTML') {
-    currentSelector = currentElem.parentNode.tagName
+  while (currentElem.tagName !== 'HTML') {
+    if (currentElem.id.length > 0) {
+      currentResult.unshift(`#${currentElem.id}`)
+      break
+    }
 
-    currentElem = currentElem.parentNode
-    let elemPosition = Array.prototype.indexOf.call(currentElem.parentNode.children, currentElem) + 1
-    currentSelector += `:nth-child(${elemPosition})`
-    let attrsSelector = Array.prototype.map
-      .call(currentElem.attributes, (el) => `[${el.nodeName}='${el.nodeValue}']`)
-      .join('')
-    currentSelector += attrsSelector
+    currentSelector = currentElem.tagName.toLowerCase()
+    let classSelector = Array.prototype.join.call(currentElem.classList, '.')
+    if (classSelector.length > 0) {
+      currentSelector += `.${classSelector}`
+    }
+
+    let childElementCount = currentElem.parentNode.childElementCount
+    if (childElementCount > 1) {
+      let elemPosition = Array.prototype.indexOf.call(currentElem.parentNode.children, currentElem) + 1
+      if (elemPosition === 1) {
+        currentSelector += ':first-child'
+      } else if (elemPosition === childElementCount) {
+        currentSelector += ':last-child'
+      } else {
+        currentSelector += `:nth-child(${elemPosition})`
+      }
+    }
     currentResult.unshift(currentSelector)
+    currentElem = currentElem.parentNode
   }
 
   return currentResult.join(' ')
